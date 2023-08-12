edc2d604dcbc4f0f92ac874c7ebe75bb6b4fe3e5
Duc Nguyen <ntrungduc228@gmail.com>
2023-08-12 17:26:22 +0700

    build appointment vs attandance model

---
+++ src/main/java/tourguide/model/Attandance.java
@@ -0,0 +1,31 @@
+package tourguide.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Attandance extends TimeStamps{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="userId", referencedColumnName = "id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name="destinationId", referencedColumnName = "id")
+    private Destination destination;
+
+    @ManyToOne
+    @JoinColumn(name="appointmentId", referencedColumnName = "id")
+    private Appoinment appoinment;
+
+    private Boolean isAttend;
+}
